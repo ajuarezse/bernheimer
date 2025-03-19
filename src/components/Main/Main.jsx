0cf230da6563a63f0731c79c9e27ca2ef2baa8f7
ajuarezse <ajuarez.se@gmail.com>
2025-03-19 16:28:23 -0700

    create contect section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,22 @@ function Main() {
   return (
     <main className="main">
       <h1 className="main__title">Hi! I'm Alan, and I live in Berkeley</h1>
+      <div className="main__content">
+        <div className="main__text">
+          <p className="main__text">
+            The quick brown fox jumped over the lazy dog.
+          </p>
+          <p className="main__text">
+            The quick brown fox jumped over the lazy dog.
+          </p>
+          <p className="main__text">
+            The quick brown fox jumped over the lazy dog.
+          </p>
+          <p className="main__text">
+            The quick brown fox jumped over the lazy dog.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
