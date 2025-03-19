7d21a381b2ca1e37a208343b7a82943f05b43317
ajuarezse <ajuarez.se@gmail.com>
2025-03-18 22:15:11 -0700

    adjust dimenstions

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,11 @@
 import "./Main.css";
 
 function Main() {
-  return <p>Main</p>;
+  return (
+    <main className="main">
+      <h1 className="main__title">Hi! I'm Alan, and I live in Berkeley</h1>
+    </main>
+  );
 }
 
 export default Main;
