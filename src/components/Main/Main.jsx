24bb60d07ea61bf92a7550ea2d3a104e84bf61dd
ajuarezse <ajuarez.se@gmail.com>
2025-03-20 21:17:11 -0700

    center content

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,12 +7,12 @@ function Main() {
       <h1 className="main__title">Hi! I'm Alan, and I live in Berkeley</h1>
       <div className="main__content">
         <div className="main__text">
-          <p className="main__text">
+          <p className="main__paragraph">
             Alan Bernheimer (born 1948 in New York City) is an American poet,
             often associated with the San Francisco Language poets and the New
             York School poets.
           </p>
-          <p className="main__text">
+          <p className="main__paragraph">
             He attended Horace Mann School, and graduated in 1970 from Yale
             College, where he became friends with poets Steve Benson, Kit
             Robinson, Rodger Kamenetz, and Alex Smith and studied literature
@@ -20,7 +20,7 @@ function Main() {
             Berrigan, Peter Schjeldahl, and Bill Berkson. He was a member of
             Manuscript Society in his senior year.
           </p>
-          <p className="main__text">
+          <p className="main__paragraph">
             He continued his association with the New York School poets and the
             St. Mark's Poetry Project for several years, and moved to San
             Francisco in 1976, where through Benson and Robinson he met other
@@ -29,7 +29,7 @@ function Main() {
             Watten—who would soon become known as the San Francisco Language
             poets.
           </p>
-          <p className="main__text">
+          <p className="main__paragraph">
             Bernheimer worked as a corporate communications executive for Bay
             Area technology and solar companies.[5] He is married to Melissa
             Riley, a former San Francisco public librarian and
