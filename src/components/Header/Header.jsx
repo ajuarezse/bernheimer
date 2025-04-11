76796c31ac72c8abb7ba716ae870ef225189e9dd
ajuarezse <ajuarez.se@gmail.com>
2025-04-11 11:53:19 -0700

    add header grid nav animations and remove column gap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,22 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <a href="#home" className="header__logo">
-        Alan Bernheimer
-      </a>
+      <div className="header__grid">
+        <a href="#home" className="header__logo">
+          Alan Bernheimer
+        </a>
+        <nav className="header__nav">
+          <a href="#about" className="header__nav-link">
+            About Me
+          </a>
+          <a href="#projects" className="header__nav-link">
+            Projects
+          </a>
+          <a href="#photos" className="header__nav-link">
+            Photos
+          </a>
+        </nav>
+      </div>
     </header>
   );
 }
