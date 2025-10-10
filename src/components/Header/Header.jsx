d2a1697e8413ea1ec02735477c972ef3a8a4455a
ajuarezse <ajuarez.se@gmail.com>
2025-10-10 13:49:04 -0700

    refactor Header component to conditionally render navigation links based on the current page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,34 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
   return (
     <header className="header">
       <div className="header__grid">
         <NavLink to="/" className="header__logo">
           AB
         </NavLink>
-        <nav className="header__nav">
-          <NavLink to="/about" className="header__nav-link">
-            About
-          </NavLink>
-          <NavLink to="/poetry" className="header__nav-link">
-            Poetry
-          </NavLink>
-          <NavLink to="/prose" className="header__nav-link">
-            Prose
-          </NavLink>
-          <NavLink to="/reviews" className="header__nav-link">
-            Reviews
-          </NavLink>
-          <NavLink to="/translations" className="header__nav-link">
-            Translations
-          </NavLink>
-        </nav>
+        {!isHomePage && (
+          <nav className="header__nav">
+            <NavLink to="/about" className="header__nav-link">
+              About
+            </NavLink>
+            <NavLink to="/poetry" className="header__nav-link">
+              Poetry
+            </NavLink>
+            <NavLink to="/prose" className="header__nav-link">
+              Prose
+            </NavLink>
+            <NavLink to="/reviews" className="header__nav-link">
+              Reviews
+            </NavLink>
+            <NavLink to="/translations" className="header__nav-link">
+              Translations
+            </NavLink>
+          </nav>
+        )}
       </div>
     </header>
   );
