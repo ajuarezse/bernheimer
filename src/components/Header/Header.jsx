7b85b1d599d2c5bd9d2e15d4399c581aec6f0f1f
ajuarezse <ajuarez.se@gmail.com>
2025-10-10 14:27:22 -0700

    refactor Header component to use flexbox for layout and improve navigation visibility based on the current page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,29 +6,40 @@ function Header() {
   const isHomePage = location.pathname === "/";
   return (
     <header className="header">
-      <div className="header__grid">
-        <NavLink to="/" className="header__logo">
+      <div
+        className={`header__grid ${
+          !isHomePage ? "header__grid--with-nav" : ""
+        }`}
+      >
+        <NavLink
+          to="/"
+          className={`header__logo ${
+            isHomePage ? "header__logo--centered" : "header__logo--with-nav"
+          }`}
+        >
           AB
         </NavLink>
-        {!isHomePage && (
-          <nav className="header__nav">
-            <NavLink to="/about" className="header__nav-link">
-              About
-            </NavLink>
-            <NavLink to="/poetry" className="header__nav-link">
-              Poetry
-            </NavLink>
-            <NavLink to="/prose" className="header__nav-link">
-              Prose
-            </NavLink>
-            <NavLink to="/reviews" className="header__nav-link">
-              Reviews
-            </NavLink>
-            <NavLink to="/translations" className="header__nav-link">
-              Translations
-            </NavLink>
-          </nav>
-        )}
+        <nav
+          className={`header__nav ${
+            isHomePage ? "header__nav--hidden" : "header__nav--visible"
+          }`}
+        >
+          <NavLink to="/about" className="header__nav-link">
+            About
+          </NavLink>
+          <NavLink to="/poetry" className="header__nav-link">
+            Poetry
+          </NavLink>
+          <NavLink to="/prose" className="header__nav-link">
+            Prose
+          </NavLink>
+          <NavLink to="/reviews" className="header__nav-link">
+            Reviews
+          </NavLink>
+          <NavLink to="/translations" className="header__nav-link">
+            Translations
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
