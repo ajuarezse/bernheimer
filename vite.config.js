91b09a212c6d5fd787aa45c5db850aabcf64ef94
ajuarezse <ajuarez.se@gmail.com>
2026-02-24 20:28:45 -0800

    configure custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
-    base: command === "serve" ? "/" : "/bernheimer/",
+    base: "/",
     server: {
       port: 3000,
     },
